59447c5fbacd9ed6bc3a65053dfcf1848280d733
Suri Thirupurnthakam <THIRUPS@ccf.org>
2017-10-10 12:15:23 -0400

    deleted unwanted folders

--- ImageUpload/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace ImageUpload.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- ImageUpload/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace ImageUpload.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
